1d457ab259908300aa448d201dbb453ab58ae1cb
Casey Jacuzzi <62916413+ccjacuzzi@users.noreply.github.com>
2023-02-26 20:39:52 -0600

    TODO#2 Complete - Changed the attributes to private, then defined class constructor and getters and setters.

--- app/src/main/java/com/ualr/simpletasklist/model/Task.java
+++ app/src/main/java/com/ualr/simpletasklist/model/Task.java
@@ -2,12 +2,35 @@ package com.ualr.simpletasklist.model;
 
 public class Task {
 
+
     // TODO 01. Define two attributes for the Task class: one to store the task description and a second one that
     //  indicates whether the task is done or not
-    public String taskDescription;  // A description of the task
-    public Boolean isComplete = false; // A boolean variable to track if the task is complete (true)
+    private String taskDescription;  // A description of the task
+    private Boolean isComplete = false; // A boolean variable to track if the task is complete (true)
 
 
     // TODO 02. Define the class constructor and the corresponding getters and setters.
+    // The constructor will create a new instance of the Task class.
+    // The taskDescription attribute will be set to the value of the string parameter newDescription
+    public Task(String newDescription){
+        taskDescription = newDescription;
+    }
+    // Getter and Setter: taskDescription
+    public String getTaskDescription(){
+        return taskDescription;
+    }
+
+    public void setTaskDescription(String taskDescription) {
+        this.taskDescription = taskDescription; // "this" refers to the current instance of the Task class
+    }
+
+    // Getter and Setter: isComplete
+    public Boolean getIsComplete(){
+        return isComplete;
+    }
+
+    public void setIsComplete(Boolean isComplete){
+        this.isComplete = isComplete;
+    }
 
 }
