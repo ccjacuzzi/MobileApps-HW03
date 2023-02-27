b17ea0017af7355f0edbb3a73412172e04ebc628
Casey Jacuzzi <62916413+ccjacuzzi@users.noreply.github.com>
2023-02-26 20:24:49 -0600

    TODO 1 Completed - Attributes added to the Task Class

--- app/src/main/java/com/ualr/simpletasklist/model/Task.java
+++ app/src/main/java/com/ualr/simpletasklist/model/Task.java
@@ -4,6 +4,9 @@ public class Task {
 
     // TODO 01. Define two attributes for the Task class: one to store the task description and a second one that
     //  indicates whether the task is done or not
+    public String taskDescription;  // A description of the task
+    public Boolean isComplete = false; // A boolean variable to track if the task is complete (true)
+
 
     // TODO 02. Define the class constructor and the corresponding getters and setters.
 
